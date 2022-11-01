5b529355bbe893b3540a01f5b6a743be53fcc8c4
roneycm <roneychristian159@gmail.com>
2022-11-01 01:00:27 -0300

    starting nlw project

---
+++ copa/server/src/server.ts
@@ -0,0 +1,26 @@
+import fastify from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+    log: ["query", "info", "warn"]
+});
+
+async function bootstrap() {
+    const app = fastify({
+        logger: true
+    });
+
+    await app.register(fastifyCors, {
+        origin: true
+        });
+
+    app.get("/pools/count", async () => {
+        const count = await prisma.pool.count();
+        return { count };
+    });
+
+    await app.listen({ port: 3000, host: "0.0.0" });
+}
+
+bootstrap();
