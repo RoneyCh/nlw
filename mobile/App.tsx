aaec2bd10232d464e008286e102cc8a1b4179926
roneycm <roneychristian159@gmail.com>
2022-11-02 18:48:11 -0300

    setting up mobile environment

---
+++ mobile/App.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { Center, NativeBaseProvider, Text } from 'native-base';
+import { THEME } from './src/styles/theme';
+
+export default function App() {
+  return (
+    <NativeBaseProvider theme={THEME}>
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24}>Hello World!</Text>
+      <StatusBar style="auto" />
+    </Center>
+    </NativeBaseProvider>
+  );
+}
+
